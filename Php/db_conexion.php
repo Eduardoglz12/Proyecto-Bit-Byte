96d56bdb0b132930d9c98a01d66329257e0f0a29
Eduardoglz12 <eduardoeuthimiusglz@gmail.com>
2025-11-01 23:36:27 -0600

    Prueba Deploy

--- Php/db_conexion.php
+++ Php/db_conexion.php
@@ -1,18 +1,18 @@
 <?php
-// Variables base de datos 
-$dbhost = "localhost";
-$dbuser = "root";
-$dbpass = "";
-$dbname = "bitandbyte";
-$dbport = "3306";
+// Lee las credenciales de las variables de entorno de Railway
+$db_host = getenv('MYSQLHOST');
+$db_user = getenv('MYSQLUSER');
+$db_pass = getenv('MYSQLPASSWORD');
+$db_name = getenv('MYSQLDATABASE');
+$db_port = getenv('MYSQLPORT');
 
-// Creamos la conexion
-$conexion = new mysqli($dbhost, $dbuser, $dbpass, $dbname, $dbport);
+// Conexión a la base de datos
+$conexion = new mysqli($db_host, $db_user, $db_pass, $db_name, $db_port);
 
 // Verificar conexión
 if ($conexion->connect_error) {
     die("Error de conexión: " . $conexion->connect_error);
 }
 
-$conexion->set_charset("utf8");
+$conexion->set_charset("utf8mb4");
 ?>
