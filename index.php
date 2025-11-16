871b5902a9a00fbdbc9fbd8608695d952c3c953a
Eduardoglz12 <eduardoeuthimiusglz@gmail.com>
2025-11-16 17:19:20 -0600

    Prueba banner png

--- index.php
+++ index.php
@@ -172,7 +172,7 @@ if (isset($_SESSION['usr_user'])) {
 
             <article class="noticias">
                 <div class="hero-slider">
-                    <div><img src="img/banner1.webp" alt="Banner Promoción 1"></div>
+                    <div><img src="img/banner1.png" alt="Banner Promoción 1"></div>
                     <div><img src="img/banner2.webp" alt="Banner Promoción 2"></div>
                     <div><img src="img/banner3.webp" alt="Banner Promoción 3"></div>
                     <div><img src="img/banner4.webp" alt="Banner Promoción 4"></div>
