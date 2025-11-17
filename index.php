8f0634e41fb1e618403e194ae5dbbfa47a36c3ab
Eduardoglz12 <eduardoeuthimiusglz@gmail.com>
2025-11-16 18:14:06 -0600

    Prueba slider 2

--- index.php
+++ index.php
@@ -174,9 +174,8 @@ if (isset($_SESSION['usr_user'])) {
                 <div class="hero-slider">
                     <div><img src="img/banner1.png" alt="Banner Promoción 1"></div>
                     <div><img src="img/banner2.webp" alt="Banner Promoción 2"></div>
-                    <div><img src="img/banner3.webp" alt="Banner Promoción 3"></div>
-                    <div><img src="img/banner4.webp" alt="Banner Promoción 4"></div>
-                    <div><img src="img/banner5.webp" alt="Banner Promoción 5"></div>
+                    
+                    
                 </div>
             </article>
 
